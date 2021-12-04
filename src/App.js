7f71c9b7144d0f7a9b4350fb2b75cb1da5265b1d
Rox <you@example.com>
2021-12-04 18:56:31 +0100

    FoodBox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react'
+import FoodBox from './components/FoodBox'
+import { Row, Divider, Button } from "antd";
+import "antd/dist/antd.css";
+
+
 
 function App() {
+  // const [foodList, setFoodList] = useState(foods)
+ 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Statessss</h1>
+
+
+     <Button> Hide Form / Add New Food </Button>
+
+     <Divider>Food List</Divider>
+
+     <Row style={{ width: "100%", justifyContent: "center" }}> 
+        {foods.map(food => {
+          return(
+              <FoodBox food={food} /> 
+          )
+        })}
+      </Row>
     </div>
   );
 }
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,22 @@
+import { Divider, Input } from "antd";
+
+function AddFoodForm(props) {
+  return (
+    <form>
+      <Divider>Add Food Entry</Divider>
+      
+      <label>Name</label>
+      <Input value={undefined} type="text" onChange={() => {}} />
+
+      <label>Image</label>
+
+      <label>Calories</label>
+
+      <label>Servings</label>
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;
